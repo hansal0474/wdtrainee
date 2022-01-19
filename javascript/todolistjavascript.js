36b01f3a684f044813e270c5e264bcc9742d5e60
Hansal <wd27.department@gmail.com>
2022-01-19 12:12:57 -0600

    calc

---
+++ javascript/todolistjavascript.js
@@ -0,0 +1,47 @@
+function add() {
+    let inputdata = document.getElementById("todoinputid").value;
+
+    if (inputdata === "") {
+        alert("ENTER THINGS TO DO")  
+    }else{
+        addfunction();
+    }
+    function addfunction() {
+        //input data
+        let inputdata = document.getElementById("todoinputid").value;
+        //list value
+        const listitem = document.createElement("li");
+        const itemvalue = document.createTextNode(inputdata);
+        listitem.appendChild(itemvalue);
+
+        //list selection
+        const list = document.getElementById("list");
+        list.appendChild(listitem);
+        //deletebutton
+        const deletebutton = document.createElement("button");
+        const buttonvalue = document.createTextNode("delete");
+        deletebutton.appendChild(buttonvalue);
+        const buttonstore = document.getElementById("buttondata");
+        buttonstore.appendChild(deletebutton);
+        //delete list
+        deletebutton.onclick = function () {
+            listitem.style.display = "none";
+            deletebutton.style.display = "none";
+        }
+        document.getElementById("todoinputid").value = "";
+
+
+        //css by javascript
+        deletebutton.style.color = "gray"; 
+        deletebutton.style.borderRadius = "10px";
+        deletebutton.style.padding = "5px";
+        deletebutton.style.border = "none";
+        deletebutton.style.display = "block";
+        deletebutton.style.height = "30px";
+        deletebutton.style.width = "50px";
+        deletebutton.style.marginTop = "10px";
+        listitem.style.height = "30px";
+        listitem.style.width = "50px";
+        listitem.style.marginTop = "10px";  
+    }   
+}
